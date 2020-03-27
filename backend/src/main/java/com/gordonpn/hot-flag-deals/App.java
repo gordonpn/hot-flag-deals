659cecc06cd82e70f90da282697715cbb91faf51
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-03-26 20:12:01 -0400

    chore: intial commit with boilerplate code

---
+++ backend/src/main/java/com/gordonpn/hot-flag-deals/App.java
@@ -0,0 +1,13 @@
+package com.gordonpn.hot-flag-deals;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
