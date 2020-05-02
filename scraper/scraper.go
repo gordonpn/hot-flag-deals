a9c27b07a2002fdc46ca5e58923410b144855876
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-05-02 19:41:44 -0400

    feat(scraper): more parsing
    * convert strings to ints where it makes sense
    * get the retailer and surround with square brackets to match with the RFD
    style

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -3,25 +3,27 @@ package main
 import (
   "fmt"
   "github.com/gocolly/colly"
+  "strconv"
   "strings"
 )
 
 type thread struct {
-  Id         string
+  Id         int
   Title      string
   Link       string
-  Posts      string
-  Votes      string
-  Views      string
+  Posts      int
+  Votes      int
+  Views      int
   DatePosted string
 }
 
-// todo restructure this correctly
+// todo parse date to a type compatible with postgresql
 // todo connect to postgresql db to insert/update
 // todo automate the scraper
 // todo set up docker container
 
 func main() {
+  getPosts()
 }
 
 func getPosts() (threads []thread) {
@@ -33,14 +35,31 @@ func getPosts() (threads []thread) {
     selector := fmt.Sprintf("#partition_forums > div > div.primary_content > div.forumbg > div > ul.topiclist.topics.with_categories > li:nth-child(%d)", i)
     collector.OnHTML(selector, func(element *colly.HTMLElement) {
       temp := thread{}
-      temp.Id = strings.TrimSpace(element.Attr("data-thread-id"))
-      titleSelector := "div > div.thread_info > div.thread_info_main.postvoting_enabled > div > h3 > a.topic_title_link"
-      temp.Title = strings.TrimSpace(element.ChildText(titleSelector))
-      temp.Link = fmt.Sprintf("%s/%s", "https://forums.redflagdeals.com/", strings.TrimSpace(element.ChildAttr(titleSelector, "href")))
-      temp.Posts = strings.TrimSpace(element.ChildText("div > div.posts"))
-      temp.Votes = strings.TrimSpace(element.ChildText("div > div.thread_info > div.thread_info_main.postvoting_enabled > div > div > dl > dd"))
-      temp.Views = strings.TrimSpace(element.ChildText("div > div.views"))
-      temp.DatePosted = strings.TrimSpace(element.ChildText("div > div.thread_info > div.thread_info_main.postvoting_enabled > div > div > div > span.first-post-time"))
+
+      titleSelector := "div > div.thread_info > div.thread_info_main.postvoting_enabled > div > h3"
+      dateSelector := "div > div.thread_info > div.thread_info_main.postvoting_enabled > div > div > div > span.first-post-time"
+      linkSelector := "div > div.thread_info > div.thread_info_main.postvoting_enabled > div > h3 > a.topic_title_link"
+      linkPrefix := "https://forums.redflagdeals.com"
+
+      retailer := element.ChildText("div > div.thread_info > div.thread_info_main.postvoting_enabled > div > h3 > a.topictitle_retailer")
+      id := StrToInt(element.Attr("data-thread-id"))
+      posts := StrToInt(element.ChildText("div > div.posts"))
+      votes := StrToInt(element.ChildText("div > div.thread_info > div.thread_info_main.postvoting_enabled > div > div > dl > dd"))
+      views := StrToInt(element.ChildText("div > div.views"))
+      title := strings.TrimSpace(element.ChildText(titleSelector))
+      title = strings.ReplaceAll(title, "\n", "")
+
+      temp.Id = id
+      if len(retailer) > 0 {
+        temp.Title = fmt.Sprintf("[%s] %s", retailer, title)
+      } else {
+        temp.Title = title
+      }
+      temp.Link = fmt.Sprintf("%s%s", linkPrefix, strings.TrimSpace(element.ChildAttr(linkSelector, "href")))
+      temp.Posts = posts
+      temp.Votes = votes
+      temp.Views = views
+      temp.DatePosted = strings.TrimSpace(element.ChildText(dateSelector))
       threads = append(threads, temp)
     })
   }
@@ -49,10 +68,31 @@ func getPosts() (threads []thread) {
     fmt.Println("Visiting", request.URL.String())
   })
 
-  for i := 1; i <= 10; i++ {
+  for i := 1; i <= 1; i++ {
     url := fmt.Sprintf("https://forums.redflagdeals.com/hot-deals-f9/%d", i)
     collector.Visit(url)
   }
 
+  fmt.Println(threads)
+
+  return
+}
+
+func StrToInt(str string) (i int) {
+  str = strings.TrimSpace(str)
+  if len(str) < 1 {
+    return 0
+  }
+  if strings.Contains(str, ",") {
+    str = strings.Replace(str, ",", "", -1)
+  }
+  if strings.Contains(str, "+") {
+    str = strings.Replace(str, "+", "", -1)
+  }
+  nonFractionalPart := strings.Split(str, ".")
+  i, err := strconv.Atoi(nonFractionalPart[0])
+  if err != nil {
+    panic(err)
+  }
   return
 }
