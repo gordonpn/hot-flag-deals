439854f7358e601022c06b1df9de521621c3dad7
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-05-10 20:49:40 -0400

    chore: ignore error if removing container that is already removed

--- .docker/scripts/deploy_dev.sh
+++ .docker/scripts/deploy_dev.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 docker container stop hotdeals_postgres
 docker container stop hotdeals_scraper
 docker container stop hotdeals_mailer
-docker container rm hotdeals_postgres
-docker container rm hotdeals_scraper
-docker container rm hotdeals_mailer
+docker container rm hotdeals_postgres || true
+docker container rm hotdeals_scraper || true
+docker container rm hotdeals_mailer || true
 docker-compose -f /drone/src/docker-compose.yml -f /drone/src/docker-compose.dev.yml up --detach --build
--- .docker/scripts/deploy_prod.sh
+++ .docker/scripts/deploy_prod.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 docker container stop hotdeals_postgres-dev
 docker container stop hotdeals_scraper-dev
 docker container stop hotdeals_mailer-dev
-docker container rm hotdeals_postgres-dev
-docker container rm hotdeals_scraper-dev
-docker container rm hotdeals_mailer-dev
+docker container rm hotdeals_postgres-dev || true
+docker container rm hotdeals_scraper-dev || true
+docker container rm hotdeals_mailer-dev || true
 docker-compose -f /drone/src/docker-compose.yml -f /drone/src/docker-compose.prod.yml up --detach --build
