bd4856675d5792c387431990db106719758880ab
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-06-06 13:15:44 -0400

    ci: add security pipeline step scan with gosec
    refs:
    issue #14

--- .docker/scripts/build.sh
+++ .docker/scripts/build.sh
@@ -4,7 +4,7 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx rm builder || true
 docker buildx create --name builder --driver docker-container --use
 docker buildx inspect --bootstrap
-cd /drone/src/mailer || exit
+cd /drone/src/mailer || exit 1
 docker buildx build -t gordonpn/hotdeals-mailer:latest --platform linux/amd64,linux/arm64 --push .
-cd /drone/src/scraper || exit
+cd /drone/src/scraper || exit 1
 docker buildx build -t gordonpn/hotdeals-scraper:latest --platform linux/amd64,linux/arm64 --push .
