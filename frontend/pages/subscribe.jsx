3754b710d8c1545193d215103c2452b3aef9a319
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-06-08 20:14:25 -0400

    feat(frontend): add recaptcha and validation for form
    refs:
    issue #7

--- frontend/pages/subscribe.jsx
+++ frontend/pages/subscribe.jsx
@@ -1,19 +1,21 @@
 import Copyright from "../src/Copyright";
 import {
-  Container,
   Box,
   Button,
+  Container,
   LinearProgress,
   Typography,
 } from "@material-ui/core";
 import React, { useState } from "react";
 import Header from "../src/Header";
 import Spacer from "../src/Spacer";
-import { Formik, Form, Field } from "formik";
+import { Field, Form, Formik } from "formik";
 import { TextField } from "formik-material-ui";
 import { makeStyles } from "@material-ui/core/styles";
 import MuiLink from "@material-ui/core/Link";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ReCAPTCHA from "react-google-recaptcha";
+import * as Yup from "yup";
 
 const useStyles = makeStyles(() => ({
   alignItemsAndJustifyContent: {
@@ -31,6 +33,7 @@ export default function Subscribe() {
   const classes = useStyles();
   const [submitted, setSubmitted] = useState(false);
   const [name, setName] = useState("");
+
   return (
     <Container>
       <Box my={4}>
@@ -57,35 +60,32 @@ export default function Subscribe() {
             initialValues={{
               name: "",
               email: "",
+              recaptcha: "",
             }}
-            validate={(values) => {
-              const errors = {};
-              if (!values.email) {
-                errors.email = "Required";
-              } else if (
-                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-              ) {
-                errors.email = "Invalid email address";
-              } else if (
-                !/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/i.test(values.name)
-              ) {
-                errors.name = "Invalid name";
-              }
-              return errors;
-            }}
+            validationSchema={Yup.object().shape({
+              name: Yup.string().matches(
+                /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
+                "Invalid name"
+              ),
+              email: Yup.string()
+                .email("Invalid email")
+                .required("Email required"),
+              recaptcha: Yup.string().required("recaptcha required"),
+            })}
             onSubmit={(values, { setSubmitting }) => {
               setSubmitted(true);
               setName(values.name);
               setSubmitting(false);
             }}
           >
-            {({ submitForm, isSubmitting }) => (
+            {({ errors, touched, submitForm, isSubmitting, setFieldValue }) => (
               <Form>
                 <Field
                   component={TextField}
                   type="text"
                   label="Name"
                   name="name"
+                  variant="filled"
                   className={classes.fields}
                 />
                 <br />
@@ -94,10 +94,22 @@ export default function Subscribe() {
                   name="email"
                   type="email"
                   label="Email"
+                  variant="filled"
                   className={classes.fields}
                 />
                 {isSubmitting && <LinearProgress />}
                 <br />
+                <Spacer />
+                <Field name="recaptcha" style={{ display: "none" }} />
+                <ReCAPTCHA
+                  sitekey="6LdAlQEVAAAAAGKrXMMe55XXlcknuswppK9xXpUI"
+                  onChange={(value) => {
+                    setFieldValue("recaptcha", value);
+                  }}
+                />
+                {errors.recaptcha && touched.recaptcha ? (
+                  <Typography color="secondary">{errors.recaptcha}</Typography>
+                ) : null}
                 <Spacer />
                 <Button
                   variant="contained"
